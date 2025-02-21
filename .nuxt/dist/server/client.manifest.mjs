bb4ade2418e68f8b220d11dc99b9a210d91e69c6
bikiyam <kihlothager@gmail.com>
2025-02-21 03:30:50 -0800

    Initial Commit

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/USER/Desktop/projects/negestat/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{js,ts,vue}',
+    './components/**/*.{js,ts,vue}',
+    './layouts/**/*.{js,ts,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
