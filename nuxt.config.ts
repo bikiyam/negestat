58ff48443163505bffdc7b5dce4925e752fd5832
bikiyam <kihlothager@gmail.com>
2025-02-21 05:37:11 -0800

    Negestat Basics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  css: [
+    '@/assets/css/index.css',
+  ],
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/google-fonts'
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class=" w-screen h-screen flex justify-center items-center">
+        <div class="negestatcard z-30 bg-gradient-to-br from-[#010000]/90 via-red-900/40 to-[#000000]/90 w-[1000px] h-[600px] rounded-[30px] flex flex-col justify-center items-center text-center transition-transform transform hover:scale-110 hover:rotate-1 duration-500">
+            <h1 class=" text-[90px] font-bold leading-[95px] text-white">NEGESTAT</h1>
+            <h1 class=" text-[12px] font-thin">The Upcoming Big Thing</h1>
+        </div>
+        <div class="text-center font-extrabold text-[#0d0d0d] text-[20px] z-0">
+            <div class=" absolute top-36 left-48">
+                ምኒልክ ዳግማዊ
+            </div>
+            <div class=" absolute top-96 right-36">
+                ይኩኖ አምላክ
+            </div>
+            <div class=" absolute bottom-36 left-44">
+                ሱስንዮስ ቀዳማዊ
+            </div>
+            <div class=" absolute top-24 right-72">
+                ፋሲለደስ
+            </div>
+            <div class=" absolute bottom-12 right-48">
+                ቴዎድሮስ
+            </div>
+        </div>
+        
+        <div class="absolute bottom-2 right-5 text-center text-white text-[14px]">
+            <h1>Stay tuned for the updates</h1>
+        </div>
+    </div>
+
+
+</template>
+
+
+<style>
+
+.negestatcard {
+    box-shadow: 20px 20px 45px rgba(73, 73, 73, 0.15);
+    border: 2px solid black;
+}
+
+</style>
